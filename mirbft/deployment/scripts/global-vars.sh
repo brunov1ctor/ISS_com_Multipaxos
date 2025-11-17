4ac3304f780eb2f8a685b325d3e1d35fdbfc045e
Bruno Victor <brunovictormg@hotmail.com>
2025-11-16 22:32:36 -0300

    ajuste de nós remotos

--- mirbft/deployment/scripts/global-vars.sh
+++ mirbft/deployment/scripts/global-vars.sh
@@ -7,16 +7,18 @@ exp_id_digits=4
 analysis_query_params="-q queries/ethereum.sql -q queries/aggregates.sql -q queries/histograms.sql"
 
 # Private key, of which the corresponding public key needs to be an authorized ssh key at each instance.
-# (Previously uploaded to IBM Cloud and specified at instance creation in the corresponding template file)
+# (No Emulab, essa é a chave que você criou e funciona: id_ed25519_emulab)
 private_key_file="$HOME/.ssh/id_ed25519_emulab"
 
 # Options to use when communicating with the remote machines.
+# Usamos sempre o usuário Bruno e essa chave.
 ssh_options="-i $private_key_file -o User=Bruno -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o ServerAliveInterval=60"
 
 # Command to kill children of exiting scripts
 trap_exit_command='{ jobs; if [ -n "$(jobs -p)" ]; then kill $(jobs -p); fi; sleep 0.5; } > /dev/null 2>&1'
 
 # Port on which the master listens.
+# Os master_machine abaixo são usados só no modo "cloud", não no "remote" do Emulab.
 #master_machine=cloud-machine-templates/dedicated-machine-32-CPUs-32GB-RAM-lon02.cmt
 #master_machine=cloud-machine-templates/dedicated-machine-32-CPUs-32GB-RAM-ams01.cmt
 master_machine=cloud-machine-templates/dedicated-machine-32-CPUs-32GB-RAM-fra02.cmt
@@ -30,14 +32,12 @@ machine_status_poll_period=5
 open_files_limit=16384
 
 # Number of seconds to wait for a deployed instance to become ready.
-# This could be provided as the --wait option to the instance creation command,
-# but it often makes the CLI command fail (buggy implementation of either the CLI or the IBM Cloud backend).
-# Instead, the instance ready state is queried separately.
 instance_ready_timeout=600
 instance_creation_batch=64
 instance_info_file_name=cloud-instance-info
 default_instance_info=last-cloud-instance-info
 
+# Valores para deploy local (não usados no remote Emulab, mas deixamos)
 local_public_ip=127.0.0.1
 local_private_ip=127.0.0.1
 local_master_command_template_file=master-commands-template.cmd
@@ -46,35 +46,72 @@ local_master_log=master-log.log
 local_master_status_file=master-status
 local_master_ready_file=master-ready
 local_result_fetching_log=result-fetching.log
+
+###############################################################################
+# Caminhos REMOTOS (adaptados para Emulab / usuário Bruno)
+###############################################################################
+
+# Diretório base de trabalho no nó remoto (NFS compartilhado)
 remote_work_dir=/users/Bruno/iss
+
+# Arquivos auxiliares de instância (usados no modo cloud, inofensivos aqui)
 remote_instance_tag_file=$remote_work_dir/instance-tag
 remote_status_file=$remote_work_dir/status
 remote_ready_file=$remote_work_dir/master-ready
-remote_main_log=/root/main_log.log
-remote_master_log=/root/master-log.log
-remote_slave_log=/root/slave-log.log
+
+# Logs principais AGORA ficam no remote_work_dir (nada em /root)
+remote_main_log=$remote_work_dir/main_log.log
+remote_master_log=$remote_work_dir/master-log.log
+remote_slave_log=$remote_work_dir/slave-log.log
+
+# Chave privada remota (modo cloud IBM, não é usada no Emulab, mas mantida)
 remote_private_key_file=$remote_work_dir/ibmcloud-ssh-key # Key used by the instances to communicate among each other.
 remote_instance_detail_file=$remote_work_dir/instance-detail.json
 remote_user_script_body=$remote_work_dir/user-script-body.sh
 remote_user_script_uploaded=$remote_work_dir/user-script-uploaded
+
+# Arquivo de comandos do master no nó remoto
 remote_master_command_file=$remote_work_dir/master-commands.cmd
+
+# Diretório remoto onde ficam os dados do experimento (logs/resultados)
 remote_exp_dir=$remote_work_dir/current-deployment-data
+
+# Número de processos de análise contínua
 remote_analysis_processes=8
 
-remote_gopath=/root/go
+# GOPATH remoto (onde código e binários serão instalados)
+remote_gopath=/users/Bruno/go
+
+# Diretório de código remoto (repo mirbft clonado pelo deploy)
 remote_code_dir="$remote_gopath/src/github.com/hyperledger-labs/mirbft"
+
+# Diretório de configs remotas geradas
 remote_config_dir=$remote_work_dir/experiment-config
+
+# Diretório de certificados TLS
 remote_tls_directory="$remote_code_dir/tls-data"
+
+# Arquivos de logs compactados
 remote_log_archives="experiment-output-*.tar.gz"
+
+# Caminho local depois de baixar código remoto (modo cloud)
 downloaded_code_dir=github.com/hyperledger-labs/mirbft/
-# remote_delete_files must be on one line, as it is passed in a string to SSH as a command to execute
-remote_delete_files="" #"$remote_work_dir/experiment-output-*.tar.gz $remote_work_dir/experiment-output $remote_master_log $remote_slave_log $remote_status_file $remote_ready_file $remote_instance_tag_file $remote_master_command_file $remote_code_dir $remote_config_dir $remote_exp_dir"
 downloaded_gopath="remote-gopath"
 
-# OLDMIR
+# remote_delete_files deve ficar em UMA linha; deixamos vazio para não tentar remover nada em /root.
+remote_delete_files=""
+
+###############################################################################
+# OLDMIR (desnecessário pro MultiPaxos, mas deixado como no original)
+###############################################################################
+
 oldmir_git_repository=git@github.ibm.com:fabric-security-research/sbft.git
 oldmir_git_branch=mir
 oldmir_git_directory=sbft # Relative path from the Go source IBM repository dir ($GOPATH/src/github.ibm.com)
+
+###############################################################################
+# Scripts auxiliares
+###############################################################################
 
 server_bootstrap_script=server-bootstrap-script.sh
 user_script_template_slave=user-script-slave.sh.template
@@ -103,3 +140,4 @@ $local_code_dir/tracing
 $local_code_dir/util
 $local_code_dir/validator
 $local_code_dir/run-protoc.sh"
+
--- mirbft/deployment/scripts/start-master.sh
+++ mirbft/deployment/scripts/start-master.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# start-master.sh exp_data_dir master_ip
+#
+# Gera o arquivo de comandos do master, envia código/configs para o nó master
+# e inicia o discoverymaster + análise contínua.
+
 source scripts/global-vars.sh
 
 # Kill all children of this script when exiting
@@ -8,72 +13,120 @@ trap "$trap_exit_command" EXIT
 exp_data_dir=$1
 master_ip=$2
 
-# Generate final master command file
+#############################
+# 1. Gerar master-commands  #
+#############################
+
+# Estas variáveis são usadas pelo envsubst no template de comandos
 export ssh_key_file=$remote_private_key_file
 export own_public_ip=$master_ip
 export master_port
 export status_file=$remote_status_file
 export ready_file=$remote_ready_file
-envsubst '$ssh_key_file $own_public_ip $master_port $status_file $ready_file' < "$exp_data_dir/$local_master_command_template_file" > "$exp_data_dir/$local_master_command_file"
+
+# Gera o arquivo de comandos final do master a partir do template
+envsubst '$ssh_key_file $own_public_ip $master_port $status_file $ready_file' \
+  < "$exp_data_dir/$local_master_command_template_file" \
+  > "$exp_data_dir/$local_master_command_file"
+
+# Ao final da execução, escreve DONE no status_file
 echo -e "\nwrite-file $status_file DONE" >> "$exp_data_dir/$local_master_command_file"
 
-# Create remote code, config, and experiment directories
-ssh $ssh_options $master_ip "
-  mkdir -p $remote_code_dir &&
-  mkdir -p $remote_config_dir &&
-  mkdir -p $remote_exp_dir/raw-results" || exit 1
+#############################################
+# 2. Criar diretórios remotos no master     #
+#############################################
 
-# Upload code to the master
-rsync --progress -rptz -e "ssh $ssh_options" $local_code_files "$master_ip:$remote_code_dir" || exit 2
+ssh $ssh_options "$master_ip" "
+  mkdir -p \"$remote_code_dir\" &&
+  mkdir -p \"$remote_config_dir\" &&
+  mkdir -p \"$remote_exp_dir/raw-results\" &&
+  mkdir -p \"$remote_gopath/bin\" &&
+  mkdir -p \"$remote_work_dir/.cache/go-build\"
+" || exit 1
 
-# Upload config to the master
-rsync --progress -rptz -e "ssh $ssh_options" $exp_data_dir/config/* "$master_ip:$remote_config_dir" || exit 3
+#############################################
+# 3. Enviar código, configs e scripts       #
+#############################################
 
-# Upload analysis scripts and queries to the master
-rsync --progress -rptz -e "ssh $ssh_options" queries scripts "$master_ip:$remote_work_dir" || exit 4
+# Upload do código da ISS/mirbft
+rsync --progress -rptz -e "ssh $ssh_options" \
+  $local_code_files "$master_ip:$remote_code_dir" || exit 2
 
-# Upload commands to the master
-scp $ssh_options "$exp_data_dir/$local_master_command_file" "$master_ip:$remote_master_command_file" || exit 5
+# Upload das configs geradas para o master
+rsync --progress -rptz -e "ssh $ssh_options" \
+  "$exp_data_dir/config/"* "$master_ip:$remote_config_dir" || exit 3
 
-# Generate TLS certificates and compile code at the master
-ssh $ssh_options $master_ip "
-  cd $remote_tls_directory &&
-  ./generate.sh $master_ip &&
-  cd $remote_work_dir &&
-  cp -r $remote_tls_directory . &&
+# Upload de consultas e scripts de análise para o diretório de trabalho remoto
+rsync --progress -rptz -e "ssh $ssh_options" \
+  queries scripts "$master_ip:$remote_work_dir" || exit 4
+
+# Upload do arquivo de comandos do master
+scp $ssh_options \
+  "$exp_data_dir/$local_master_command_file" \
+  "$master_ip:$remote_master_command_file" || exit 5
+
+#############################################
+# 4. Gerar TLS e compilar no master         #
+#############################################
+
+ssh $ssh_options "$master_ip" "
+  set -e
+
+  # Gerar certificados TLS
+  cd \"$remote_tls_directory\" &&
+  ./generate.sh \"$master_ip\" &&
+
+  # Copiar TLS para o diretório de trabalho
+  cd \"$remote_work_dir\" &&
+  cp -r \"$remote_tls_directory\" . &&
 
   echo 'Compiling ISS.' &&
-  export PATH=\$PATH:$remote_gopath/bin:$remote_work_dir/bin &&
-  export GOPATH=$remote_work_dir/go
-  # Disabling go modles to be able to compile with new Go version (>=1.16.3)
-  export GO111MODULE=auto
-  export GOCACHE=/root/.cache/go-build
-  cd $remote_code_dir &&
+
+  # Preparar ambiente Go
+  export GOPATH=\"$remote_gopath\" &&
+  export PATH=\"\$PATH:$remote_gopath/bin:$remote_work_dir/bin\" &&
+
+  # Desabilitar módulos go para compatibilidade com versões mais novas
+  export GO111MODULE=auto &&
+  export GOCACHE=\"$remote_work_dir/.cache/go-build\" &&
+
+  # Compilar protobufs e binários
+  cd \"$remote_code_dir\" &&
   ./run-protoc.sh &&
-  go install ./cmd/..." || exit 6
-# &&
-#
-#  echo 'Cloning and compiling old Mir.' &&
-#  cd $remote_gopath/src/github.ibm.com &&
-#  cp $remote_code_dir/oldmir/oldmir-key . &&
-#  chmod 0700 oldmir-key &&
-#  rm -rf $oldmir_git_directory &&
-#  ssh-agent bash -c \"ssh-add oldmir-key; git clone $oldmir_git_repository\" &&
-#  cd $oldmir_git_directory &&
-#  git checkout $oldmir_git_branch &&
-#  go install ./server &&
-#  go install ./client" || exit 6
+  go install ./cmd/...
+" || exit 6
 
-## Run threshold signature microbenchmark
-#ssh $ssh_options $master_ip "
-#  echo 'Running threshold signature microbenchmark.' &&
-#  cd $remote_code_dir/crypto/ &&
-#  go test -bench=TBLS" || exit 7
+#############################################
+# 5. Iniciar master e análise contínua      #
+#############################################
 
-# Start master server
-echo "Starting result processor and master server."
-ssh $ssh_options $master_ip "
+echo "Starting result processor and master server on $master_ip."
+
+ssh $ssh_options "$master_ip" "
   ulimit -Sn $open_files_limit &&
-  $remote_work_dir/scripts/analyze/analyze-continuously.sh $remote_exp_dir $remote_status_file $remote_work_dir/scripts $remote_work_dir/queries $remote_gopath/bin/orderingpeer $remote_gopath/bin/orderingclient $remote_analysis_processes > $remote_exp_dir/continuous-analysis.log 2>&1 &
-  export PATH=\$PATH:$remote_gopath/bin:$remote_work_dir/bin &&
-  discoverymaster $master_port file $remote_master_command_file > $remote_master_log 2>&1 < /dev/null"
+
+  cd \"$remote_work_dir\" &&
+
+  # Inicia análise contínua em background (se script existir)
+  if [ -x \"$remote_work_dir/scripts/analyze/analyze-continuously.sh\" ]; then
+    \"$remote_work_dir/scripts/analyze/analyze-continuously.sh\" \
+      \"$remote_exp_dir\" \
+      \"$remote_status_file\" \
+      \"$remote_work_dir/scripts\" \
+      \"$remote_work_dir/queries\" \
+      \"$remote_gopath/bin/orderingpeer\" \
+      \"$remote_gopath/bin/orderingclient\" \
+      $remote_analysis_processes \
+      > \"$remote_exp_dir/continuous-analysis.log\" 2>&1 &
+  fi
+
+  # Inicia discoverymaster
+  export GOPATH=\"$remote_gopath\" &&
+  export PATH=\"\$PATH:$remote_gopath/bin:$remote_work_dir/bin\" &&
+
+  discoverymaster $master_port file \"$remote_master_command_file\" \
+    > \"$remote_master_log\" 2>&1 < /dev/null
+"
+
+echo "Master and result processor started on $master_ip."
+
