824b1188783edf27c9071a3a3a7bbfc68247ebcf
Bruno Victor <brunovictormg@hotmail.com>
2025-11-20 15:57:03 -0300

    atualizando global-vars

--- mirbft/deployment/scripts/global-vars.sh
+++ mirbft/deployment/scripts/global-vars.sh
@@ -154,6 +154,5 @@ $local_code_dir/statetransfer
 $local_code_dir/tls-data
 $local_code_dir/tracing
 $local_code_dir/util
-$local_code_dir/validator
-$local_code_dir/run-protoc.sh"
+$local_code_dir/validator"
 
