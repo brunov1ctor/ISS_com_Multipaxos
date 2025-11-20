c50178fd7b6f3770ed26a4ab0a9dd6302d0f9b29
Bruno Victor <brunovictormg@hotmail.com>
2025-11-20 16:30:30 -0300

    atualizando start-master

--- mirbft/deployment/scripts/start-master.sh
+++ mirbft/deployment/scripts/start-master.sh
@@ -21,9 +21,9 @@ envsubst '$ssh_key_file $own_public_ip $master_port $status_file $ready_file' \
 
 echo -e "\nwrite-file $status_file DONE" >> "$exp_data_dir/$local_master_command_file"
 
-# ------------------------------------------------------------------------------
-# Criar diretórios remotos de código, config e experimentos no MASTER
-# ------------------------------------------------------------------------------
+###############################################################################
+# Criar diretórios remotos do experimento no MASTER
+###############################################################################
 
 ssh $ssh_options "$master_ip" "
   mkdir -p \"$remote_code_dir\" &&
@@ -31,41 +31,42 @@ ssh $ssh_options "$master_ip" "
   mkdir -p \"$remote_exp_dir/raw-results\"
 " || exit 1
 
-# ------------------------------------------------------------------------------
-# Enviar código para o master
-# ------------------------------------------------------------------------------
+###############################################################################
+# Enviar código para o MASTER
+###############################################################################
 
 rsync --progress -rptz -e "ssh $ssh_options" \
   $local_code_files \
   "$master_ip:$remote_code_dir" || exit 2
 
-# ------------------------------------------------------------------------------
-# Enviar configs para o master
-# ------------------------------------------------------------------------------
+###############################################################################
+# Enviar configs
+###############################################################################
 
 rsync --progress -rptz -e "ssh $ssh_options" \
   "$exp_data_dir/config/"* \
   "$master_ip:$remote_config_dir" || exit 3
 
-# ------------------------------------------------------------------------------
-# Enviar scripts de análise e queries para o master
-# ------------------------------------------------------------------------------
+###############################################################################
+# Enviar scripts de análise e queries
+###############################################################################
 
 rsync --progress -rptz -e "ssh $ssh_options" \
   queries scripts \
   "$master_ip:$remote_work_dir" || exit 4
 
-# ------------------------------------------------------------------------------
-# Enviar arquivo de comandos do master
-# ------------------------------------------------------------------------------
+###############################################################################
+# Enviar master-commands.cmd
+###############################################################################
 
 scp $ssh_options \
   "$exp_data_dir/$local_master_command_file" \
   "$master_ip:$remote_master_command_file" || exit 5
 
-# ------------------------------------------------------------------------------
-# Gerar certificados TLS e compilar ISS no master
-# ------------------------------------------------------------------------------
+###############################################################################
+# Gerar TLS e Compilar ISS no MASTER
+# (REMOVIDO run-protoc.sh porque os pb.go já estão no repositório)
+###############################################################################
 
 ssh $ssh_options "$master_ip" "
   set -e
@@ -76,34 +77,25 @@ ssh $ssh_options "$master_ip" "
   cd \"$remote_work_dir\" &&
   cp -r \"$remote_tls_directory\" . &&
 
-  echo 'Compiling ISS.' &&
+  echo 'Compiling ISS (sem protoc).' &&
   export PATH=\"\$PATH:$remote_gopath/bin:$remote_work_dir/bin\" &&
   export GOPATH=\"$remote_gopath\" &&
-  export GO111MODULE=off &&
+  export GO111MODULE=auto &&
   export GOCACHE=\"$remote_work_dir/.cache/go-build\" &&
 
   cd \"$remote_code_dir\" &&
-
-  # Baixar dependências externas no GOPATH
-  go get ./... &&
-
-  # Gerar arquivos protobuf
-  ./run-protoc.sh &&
-
-  # Instalar os binários (discoverymaster, discoveryslave, orderingpeer, orderingclient, etc.)
   go install ./cmd/...
 " || exit 6
 
-# ------------------------------------------------------------------------------
-# Iniciar processador de resultados e master server
-# ------------------------------------------------------------------------------
+###############################################################################
+# Iniciar processador contínuo + master
+###############################################################################
 
 echo "Starting result processor and master server."
 ssh $ssh_options "$master_ip" "
   ulimit -Sn $open_files_limit &&
   export PATH=\"\$PATH:$remote_gopath/bin:$remote_work_dir/bin\" &&
 
-  # analisador contínuo em background
   \"$remote_work_dir/scripts/analyze/analyze-continuously.sh\" \
     \"$remote_exp_dir\" \
     \"$remote_status_file\" \
@@ -114,7 +106,6 @@ ssh $ssh_options "$master_ip" "
     $remote_analysis_processes \
     > \"$remote_exp_dir/continuous-analysis.log\" 2>&1 &
 
-  # master
   discoverymaster $master_port file \"$remote_master_command_file\" \
     > \"$remote_master_log\" 2>&1 < /dev/null
 "
