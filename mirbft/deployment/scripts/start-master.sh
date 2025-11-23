3168dfc5f926389c5d9100beb74578561a3480ea
Bruno Victor <brunovictormg@hotmail.com>
2025-11-23 13:44:35 -0300

    ajuste em stubborn

--- mirbft/deployment/scripts/start-master.sh
+++ mirbft/deployment/scripts/start-master.sh
@@ -9,7 +9,7 @@ exp_data_dir=$1
 master_ip=$2
 
 ###############################################################################
-# Gerar master-commands.cmd (já foi gerado pelo initialize-deployment.sh)
+# Gerar master-commands.cmd
 ###############################################################################
 
 export ssh_key_file=$remote_private_key_file
@@ -30,82 +30,34 @@ echo "Master command script written to $master_command_file."
 echo ""
 
 ###############################################################################
-# Copiar master-commands e config para o master
+# Copiar master-commands e config do master
 ###############################################################################
 
-echo "Copying master commands and configs."
+echo "Copying master commands and configs to master."
 
-# master-commands.cmd
+# Copia o master-commands para o master
 ./scripts/stubborn-scp.sh 10 \
   "$master_command_file" \
   "$master_ip:$remote_master_command_file" || exit 4
 
-# ATENÇÃO: o config-0000.yml fica dentro do subdiretório 'config/'
-# (deployment-data/remote-XXXX/config/config-0000.yml)
-master_config_local="$exp_data_dir/config/config-0000.yml"
+# Copia o config-0000.yml (da pasta config/) para o master, se existir
+master_local_cfg="$exp_data_dir/config/config-0000.yml"
 
-if [ ! -f "$master_config_local" ]; then
-  echo "ERRO: Arquivo de configuração do master não encontrado: $master_config_local" >&2
-  exit 4
+if [ -f "$master_local_cfg" ]; then
+  ./scripts/stubborn-scp.sh 10 \
+    "$master_local_cfg" \
+    "$master_ip:$remote_master_config_file" || exit 4
+else
+  echo "WARNING: master config file not found at $master_local_cfg. Skipping copy to master."
 fi
-
-./scripts/stubborn-scp.sh 10 \
-  "$master_config_local" \
-  "$master_ip:$remote_master_config_file" || exit 4
 
 echo "Done."
 
 ###############################################################################
-# Copiar arquivos de configuração para slaves
+# NÃO vamos copiar configs para os slaves aqui.
+# A distribuição/configuração dos slaves é tratada pelos outros scripts
+# (start-remote-slaves, deploy-remote, etc.).
 ###############################################################################
-
-echo "Copying configs to slaves."
-
-# Gera um arquivo com:
-#   <IP> <config-local> <config-remoto>
-tmp_config_scp_cmds=$(mktemp)
-
-# Para cada linha do deployment.dpl, montamos qual config enviar para qual IP
-# Formato esperado do deployment.dpl:
-#   expID slaveID slaveIP slaveRole configID ...
-while read line; do
-  # Ignorar linhas vazias/comentários
-  if [ -z "$line" ] || [[ "$line" =~ ^# ]]; then
-    continue
-  fi
-
-  exp_id=$(echo "$line"    | awk '{print $1}')
-  slave_id=$(echo "$line"  | awk '{print $2}')
-  slave_ip=$(echo "$line"  | awk '{print $3}')
-  slave_role=$(echo "$line"| awk '{print $4}')
-  config_id=$(echo "$line" | awk '{print $5}')
-
-  # Caminho local do config gerado para esse experimento
-  # IMPORTANTE: também aqui ele fica dentro de 'config/'
-  local_config_file="$exp_data_dir/config/config-$(printf "%04d" "$exp_id").yml"
-
-  # Caminho remoto do config no slave
-  # remote_slave_config_dir vem de global-vars.sh (ex.: /users/Bruno/iss/config)
-  remote_config_file="$remote_slave_config_dir/config-$config_id.yml"
-
-  echo "$slave_ip $local_config_file $remote_config_file" >> "$tmp_config_scp_cmds"
-done < "$exp_data_dir/$dpl_filename"
-
-# Agora faz o SCP de todos os configs necessários
-while read slave_ip local_cfg remote_cfg; do
-  if [ ! -f "$local_cfg" ]; then
-    echo "ERRO: Config local não encontrado: $local_cfg (slave $slave_ip)" >&2
-    exit 5
-  fi
-
-  ./scripts/stubborn-scp.sh 10 \
-    "$local_cfg" \
-    "$slave_ip:$remote_cfg" || exit 5
-done < "$tmp_config_scp_cmds"
-
-rm -f "$tmp_config_scp_cmds"
-
-echo "Configs copied."
 
 ###############################################################################
 # Iniciar processador contínuo + master
