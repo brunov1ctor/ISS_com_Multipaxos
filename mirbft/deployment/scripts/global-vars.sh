ee74e5335888b1adcbed565485cac996f9436be3
Bruno Victor <brunovictormg@hotmail.com>
2025-11-26 21:01:13 -0300

    atualizando global-vars

--- mirbft/deployment/scripts/global-vars.sh
+++ mirbft/deployment/scripts/global-vars.sh
@@ -51,7 +51,10 @@ local_result_fetching_log=result-fetching.log
 ###############################################################################
 # REMOTO (Emulab)
 ###############################################################################
+# Diretório "home" remoto do usuário Bruno em TODOS os nós
+remote_home="/users/Bruno"
 
+# Diretório raiz do ISS em cada máquina remota
 remote_work_dir="${remote_home}/iss"
 
 remote_instance_tag_file="$remote_work_dir/instance-tag"
@@ -78,6 +81,10 @@ remote_analysis_processes=0
 ###############################################################################
 
 remote_gopath="${remote_home}/go"
+
+# Caminho onde o código foi clonado no master (node-0).
+# Nos slaves você está usando apenas os binários + scripts, mas manter isso
+# aqui não atrapalha (serve para limpeza remota se necessário).
 remote_code_dir="/tmp/ISS_com_Multipaxos/mirbft"
 
 remote_config_dir="$remote_work_dir/experiment-config"
@@ -87,7 +94,17 @@ remote_log_archives="experiment-output-*.tar.gz"
 downloaded_code_dir="github.com/hyperledger-labs/mirbft/"
 downloaded_gopath="remote-gopath"
 
-remote_delete_files="$remote_work_dir/experiment-output-*.tar.gz $remote_work_dir/experiment-output $remote_master_log $remote_slave_log $remote_status_file $remote_ready_file $remote_instance_tag_file $remote_master_command_file $remote_code_dir $remote_config_dir $remote_exp_dir"
+remote_delete_files="$remote_work_dir/experiment-output-*.tar.gz \
+$remote_work_dir/experiment-output \
+$remote_master_log \
+$remote_slave_log \
+$remote_status_file \
+$remote_ready_file \
+$remote_instance_tag_file \
+$remote_master_command_file \
+$remote_code_dir \
+$remote_config_dir \
+$remote_exp_dir"
 
 ###############################################################################
 # OLDMIR (compatibilidade)
