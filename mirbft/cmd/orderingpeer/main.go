9fca79e07bfe80b9e733abc5e587173fb1cc54f9
Bruno Victor <brunovictormg@hotmail.com>
2025-12-01 20:56:41 -0300

    atualizando orderingpeer

--- mirbft/cmd/orderingpeer/main.go
+++ mirbft/cmd/orderingpeer/main.go
@@ -3,30 +3,33 @@ package main
 import (
 	"fmt"
 	"os"
+	"os/signal"
+	"path/filepath"
 	"sync"
-	"time"
+	"syscall"
 
 	"github.com/rs/zerolog"
-	logger "github.com/rs/zerolog/log"
+	"github.com/rs/zerolog/log"
 
-	"github.com/hyperledger-labs/mirbft/checkpoint"
-	"github.com/hyperledger-labs/mirbft/config"
+	"github.com/hyperledger-labs/mirbft/cmd/orderingpeer/config"
+	"github.com/hyperledger-labs/mirbft/cmd/orderingpeer/discovery"
+	"github.com/hyperledger-labs/mirbft/cmd/orderingpeer/membership"
+	"github.com/hyperledger-labs/mirbft/cmd/orderingpeer/profiling"
+	"github.com/hyperledger-labs/mirbft/cmd/orderingpeer/request"
+	"github.com/hyperledger-labs/mirbft/cmd/orderingpeer/statetransfer"
+
 	"github.com/hyperledger-labs/mirbft/crypto"
-	"github.com/hyperledger-labs/mirbft/discovery"
-	"github.com/hyperledger-labs/mirbft/manager"
-	mirlog "github.com/hyperledger-labs/mirbft/log"
-	"github.com/hyperledger-labs/mirbft/membership"
-	"github.com/hyperledger-labs/mirbft/messenger"
-	"github.com/hyperledger-labs/mirbft/orderer"
-	"github.com/hyperledger-labs/mirbft/profiling"
-	"github.com/hyperledger-labs/mirbft/request"
-	"github.com/hyperledger-labs/mirbft/statetransfer"
+	"github.com/hyperledger-labs/mirbft/logger"
+	"github.com/hyperledger-labs/mirbft/mirlog"
+	"github.com/hyperledger-labs/mirbft/modules/checkpoint"
+	"github.com/hyperledger-labs/mirbft/modules/manager"
+	"github.com/hyperledger-labs/mirbft/modules/messenger"
+	"github.com/hyperledger-labs/mirbft/modules/orderer"
 	"github.com/hyperledger-labs/mirbft/tracing"
 )
 
-// Flag indicating whether profiling is enabled.
+// profilingEnabled is a global variable indicating whether profiling is enabled.
 // Used to decide whether the tracer should shut down the process on the INT signal or not.
-// TODO: This is ugly and dirty. Implement graceful shutdown!
 var profilingEnabled = false
 
 // local interface to avoid importing manager internals
@@ -38,12 +41,33 @@ func main() {
 
 	exe, _ := os.Executable()
 	fmt.Printf("[BIN] orderingpeer exe=%s\n", exe)
+	fmt.Printf("[ARGS] len=%d args=%v\n", len(os.Args), os.Args)
+
+	if len(os.Args) < 5 {
+		fmt.Fprintf(os.Stderr,
+			"[ARGS] ERROR: expected at least 5 arguments: <configFile> <discoveryAddr> <ownPublicIP> <ownPrivateIP> [traceFile] [profDir], got %d\n",
+			len(os.Args))
+		os.Exit(1)
+	}
 
 	// Get command line arguments
 	configFileName := os.Args[1]
 	discoveryServAddr := os.Args[2]
 	ownPublicIP := os.Args[3]
 	ownPrivateIP := os.Args[4]
+
+	var traceFileName string
+	var profDir string
+
+	if len(os.Args) > 5 {
+		traceFileName = os.Args[5]
+	}
+	if len(os.Args) > 6 {
+		profDir = os.Args[6]
+	}
+
+	fmt.Printf("[ARGS] config=%s discovery=%s ownPub=%s ownPriv=%s traceFile=%s profDir=%s\n",
+		configFileName, discoveryServAddr, ownPublicIP, ownPrivateIP, traceFileName, profDir)
 
 	config.LoadFile(configFileName)
 
@@ -55,6 +79,15 @@ func main() {
 		NoColor:    true,
 		TimeFormat: "15:04:05.000",
 	})
+
+	logger.Info().
+		Str("configFile", configFileName).
+		Str("discoveryAddr", discoveryServAddr).
+		Str("ownPublicIP", ownPublicIP).
+		Str("ownPrivateIP", ownPrivateIP).
+		Str("traceFileArg", traceFileName).
+		Str("profDirArg", profDir).
+		Msg("orderingpeer starting with arguments.")
 
 	// Initialize packages that need the configuration to be loaded for initialization
 	membership.Init()
@@ -83,25 +116,51 @@ func main() {
 	if err != nil {
 		logger.Fatal().Msgf("Could not deserialize TBLS public key %s", err.Error())
 	}
-	membership.TBLSPublicKey = TBLSPubKey
 	TBLSPrivKeyShare, err := crypto.TBLSPrivKeyShareFromBytes(serializedTBLSPrivKeyShare)
 	if err != nil {
 		logger.Fatal().Msgf("Could not deserialize TBLS private key share %s", err.Error())
 	}
-	membership.TBLSPrivKeyShare = TBLSPrivKeyShare
 
-	// Start profiler if necessary
-	// ATTENTION! We first look for argument 6, and only then check argument 5
-	//            (as the presence of profiling influences setting up of tracing).
-	if len(os.Args) > 6 {
-		profilingEnabled = true // UGLY DIRTY CODE!
-		logger.Info().Msg("Profiling enabled.")
-		setUpProfiling(os.Args[6])
+	membership.TBLSPublicKey = TBLSPubKey
+	membership.TBLSPrivateKeyShare = TBLSPrivKeyShare
+
+	logger.Info().Msg("Deserialized TBLS keys.")
+
+	// Initialize channels and connect modules through them.
+	// Event dispatcher channel between different modules.
+	eventsInChan := make(chan *mirlog.EventList)
+	eventsOutChan := make(chan *mirlog.EventList)
+	membership.ManagerEventsOut = eventsInChan
+	membership.ManagerEventsIn = eventsOutChan
+	request.ManagerEventsOut = eventsInChan
+	request.ManagerEventsIn = eventsOutChan
+	statetransfer.ManagerEventsOut = eventsInChan
+	statetransfer.ManagerEventsIn = eventsOutChan
+
+	// Initialize the messenger
+	messenger.Init()
+	messenger.MainEventLog = mirlog.NewLog(eventsInChan)
+	membership.PeerConnections = messenger.PeerConnections
+	request.Messenger = messenger.DefaultMessenger
+	statetransfer.PeerCommunication = messenger.PeerCommunication
+	logger.Info().Msg("Initialized messenger.")
+
+	// Profiling (arg index 6) – influences tracing exit behaviour.
+	if profDir != "" {
+		profilingEnabled = true
+		logger.Info().Str("profDir", profDir).Msg("Profiling enabled.")
+		setUpProfiling(profDir)
+	} else {
+		logger.Info().Msg("Profiling disabled (no profDir argument).")
 	}
 
-	// Set up tracing if necessary
-	if len(os.Args) > 5 {
-		setUpTracing(os.Args[5], ownID)
+	// Set up tracing (arg index 5)
+	if traceFileName != "" {
+		logger.Info().Str("traceFile", traceFileName).Msg("Setting up tracing.")
+		ensureTraceDir(traceFileName)
+		setUpTracing(traceFileName, ownID)
+	} else {
+		logger.Warn().Msg("No trace file argument provided. Tracing will be disabled and no .trc file will be written.")
 	}
 
 	// Declare variables for component modules.
@@ -111,38 +170,64 @@ func main() {
 	var rsp *request.Responder
 
 	// Instantiate component modules (with stubs).
-	mngr = setManager(config.Config.Manager)
-	ord = setOrderer(config.Config.Orderer)
-	chkp = setCheckpointer(config.Config.Checkpointer)
+	mngr = setManager(config.Config.ManagerType)
+	chkp = setCheckpointer(config.Config.ManagerType)
 	rsp = request.NewResponder()
 
-	// Initialize modules.
-	// No outgoing messages must be produced even after initialization,
-	// but the modules must be ready to process incoming messages.
-	ord.Init(mngr)
-	chkp.Init(mngr)
+	// Connect modules through their event handlers.
+	// Using type alias in order to use the same function for both Manager and Checkpointer below.
+	// Both implement the entryHandlerRegistrar interface defined above.
+	var entryHandlerReg entryHandlerRegistrar
 
-	// >>> Registra SOMENTE o handler do orderer (o Responder lê do log global)
-	if reg, ok := mngr.(entryHandlerRegistrar); ok {
-		reg.RegisterEntryHandler(ord.HandleEntry)
+	rspRespHandler := rsp.RespHandler
+	if config.Config.ManagerType == "Dummy" {
+		rspRespHandler = func(request.IssClientID, []request.IssSeqNr, [][]byte) {}
 	}
 
-	// Register message and entry handlers
-	messenger.CheckpointMsgHandler = chkp.HandleMessage
-	messenger.OrdererMsgHandler = ord.HandleMessage
-	messenger.ClientRequestHandler = request.HandleRequest
+	// Create orderer and connect it to the other modules.
+	entryHandlerReg = mngr
+	ord = config.InitializeOrderer()
+	ord.Connect(
+		mirlog.NewRouter(entryHandlerReg.RegisterEntryHandler),
+		rspRespHandler,
+		statetransfer.NewStateReceiver(),
+	)
+	logger.Info().Str("orderer", config.Config.OrdererType).Msg("Initialized orderer.")
+
+	// Connect manager to modules
+	entryHandlerReg = chkp
+	mngr.Connect(
+		ord,
+		rsp,
+		mirlog.NewRouter(entryHandlerReg.RegisterEntryHandler),
+		messenger.LocalClientConnection,
+	)
+
+	// Connect checkpointer to modules.
+	chkp.Connect(
+		ord,
+		messenger.PeerConnections,
+	)
+
+	// Connect modules to state receiver.
+	statetransfer.OrdererConfiguration = ord.Configuration
+	statetransfer.StateReceiver = chkp
+
+	// Register handlers for messages to this node.
+	messenger.PeerMsgHandler = mngr.HandleMessage
+	messenger.ClientMsgHandler = rsp.HandleRequest
+	messenger.SuspicionTracer = ord.Tracer()
+
+	// Register handler for events to be executed.
+	mngr.SetEventHandler(eventsOutChan, mngr)
 	messenger.StateTransferMsgHandler = statetransfer.HandleMessage
 	statetransfer.OrdererEntryHandler = ord.HandleEntry
 
 	// Create wait group for all the modules that will run as separate goroutines.
-	// (Currently the graceful termination is not implemented, so waiting on wg will take forever and the process
-	// needs to be killed.)
 	wg := sync.WaitGroup{}
 	wg.Add(5) // messenger, checkpointer, orderer, manager, responder
 
 	// Start the messaging subsystem.
-	// Connect needs to come after starting the messenger which launches the gRPC server everybody connects to.
-	// Otherwise we deadlock, everybody connecting to gRPC servers that are not (and never will be) running.
 	go messenger.Start(&wg)
 	messenger.Connect()
 	logger.Info().Msg("Connected to all peers.")
@@ -165,50 +250,59 @@ func main() {
 		}
 	}
 
-	// Start all modules.
-	// The order of the calls must not matter, as they are all concurrent. If it does, it's a bug.
-	// By now all the modules must be initialized and ready to process messages.
-	// After starting, the modules will produce messages on their own.
-	go rsp.Start(&wg)  // Responder observa mirlog.Entries() e chama messenger.RespondToClient(...)
-	go chkp.Start(&wg)
-	go mngr.Start(&wg)
-	go ord.Start(&wg)
+	// Start modules
+	go mngr.Run(&wg)
+	go chkp.Run(&wg)
+	go rsp.Run(&wg)
+	go ord.Run(&wg)
 
-	// Wait for all modules to finish.
+	// Handle OS signals to allow a graceful shutdown.
+	handleSignals()
+
+	// Wait forever (graceful termination not implemented, exit on signal).
 	wg.Wait()
 }
 
-// Enables and starts the profiler of used resources.
-func setUpProfiling(outFilePrefix string) {
-	logger.Info().Msg("Setting up profiling.")
-
-	profiling.StartProfiler("cpu", outFilePrefix+".cpu", 1)
-	profiling.StartProfiler("block", outFilePrefix+".block", 1)
-	profiling.StartProfiler("mutex", outFilePrefix+".mutex", 1)
-
-	// Stop profiler on INT signal
-	// TODO: Once graceful termination is implemented, use defer StopProfiler()
-	profiling.StopOnSignal(os.Interrupt, true)
+func ensureTraceDir(traceFile string) {
+	dir := filepath.Dir(traceFile)
+	if dir == "." || dir == "" {
+		logger.Warn().Str("traceFile", traceFile).Msg("Trace file has no directory component.")
+		return
+	}
+	err := os.MkdirAll(dir, 0o755)
+	if err != nil {
+		logger.Error().Str("dir", dir).Err(err).Msg("Failed to create trace directory.")
+	} else {
+		logger.Info().Str("dir", dir).Msg("Ensured trace directory exists.")
+	}
 }
 
-// Sets up tracing of events.
+// handleSignals installs a handler for SIGINT and SIGTERM, just for logging.
+func handleSignals() {
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		for sig := range sigChan {
+			logger.Info().Str("signal", sig.String()).Msg("Received OS signal in orderingpeer.")
+		}
+	}()
+}
+
+func setUpProfiling(profDir string) {
+	err := profiling.StartCPUTracing(profDir)
+	if err != nil {
+		logger.Error().Err(err).Msg("Failed to start CPU profiling.")
+	} else {
+		logger.Info().Str("profDir", profDir).Msg("CPU profiling started.")
+	}
+}
+
 func setUpTracing(outFileName string, ownID int32) {
-	// Initialize tracing with output file name given at command line
+	logger.Info().Str("traceFile", outFileName).Int32("ownID", ownID).Msg("Initializing tracing.")
 	tracing.MainTrace.Start(outFileName, ownID)
-
-	// TODO: Move the CPU tracing to a more appropriate place
-	//       For now, it is here, as it depends on tracing being enabled.
-	profiling.StartCPUTracing(tracing.MainTrace, 500*time.Millisecond)
-
-	// Stop tracing on INT signal.
-	// TODO: Once graceful termination is implemented, use defer
-	// The second parameter determines whether to shut down the process after stopping the tracing.
-	// If profiling is on, the profiler will do the job.
-	// TODO: ATTENTION! Implement some synchronization here, otherwise the profiler might exit the process before
-	//                  the tracer is done flushing its buffers.
 	tracing.MainTrace.StopOnSignal(os.Interrupt, !profilingEnabled)
-
-	logger.Info().Str("traceFile", outFileName).Msg("Started tracing.")
+	logger.Info().Str("traceFile", outFileName).Msg("Started tracing (StopOnSignal registered).")
 }
 
 func setManager(managerType string) (mngr manager.Manager) {
@@ -221,26 +315,6 @@ func setManager(managerType string) (mngr manager.Manager) {
 		logger.Fatal().Msg("Unsupported manager type")
 	}
 	return mngr
-}
-
-func setOrderer(ordererType string) (ord orderer.Orderer) {
-	switch ordererType {
-	case "Dummy":
-		ord = &orderer.DummyOrderer{}
-	case "Pbft":
-		ord = &orderer.PbftOrderer{}
-	case "HotStuff":
-		ord = &orderer.HotStuffOrderer{}
-	case "Raft":
-		ord = &orderer.RaftOrderer{}
-	case "MultiPaxos":
-		ord = &orderer.MultiPaxosOrderer{}
-	case "MultiPaxosMulticast":
-    		ord = &orderer.MultiPaxosMulticastOrderer{}
-	default:
-		logger.Fatal().Msg("Unsupported orderer type")
-	}
-	return ord
 }
 
 func setCheckpointer(managerType string) (chkp checkpoint.Checkpointer) {
