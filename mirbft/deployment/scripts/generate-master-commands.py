9a0abc6b9f5bcd18bd86f03bc9de5634105b10e1
Bruno Victor <brunovictormg@hotmail.com>
2025-12-21 16:40:37 -0300

    atualizando generate-master

--- mirbft/deployment/scripts/generate-master-commands.py
+++ mirbft/deployment/scripts/generate-master-commands.py
@@ -8,7 +8,7 @@ SIGNAL_DELAY = "5s"
 STOP_SLAVES_DELAY = "3s"
 SCP_RETRY_COUNT = "10"
 MASTER_CONFIG_DIR = "experiment-config"
-MASTER_EXP_DIR = "iss/current-deployment-data"
+MASTER_EXP_DIR="current-deployment-data"
 SLAVE_CONFIG_FILE = "config/config.yml"
 OLDMIR_SERVER_CONFIG = "config/oldmir-config-server.yml"
 OLDMIR_CLIENT_CONFIG = "config/oldmir-config-client.yml"
@@ -32,7 +32,7 @@ def waitForSlaves(slaves):
     for s in slaves:
         output("wait for slaves {0} {1}".format(s, numSlaves[s]))
     output("")
-
+    
 
 def createLogDir(expID):
     output("# Create log directory.")
@@ -51,13 +51,31 @@ def createLocalLogDir(expID):
 def pushConfigFiles(expID, slaves):
     output("# Push config files.")
     for s, configFile in slaves.items():
+        if deplType == "remote":
+            scp_cmd = (
+                "exec-start {0} scp-output-{1}-config.log stubborn-scp.sh {5} "
+                "$own_public_ip:{2}/{3} {4}"
+            )
+        else:
+            scp_cmd = (
+                "exec-start {0} scp-output-{1}-config.log stubborn-scp.sh {5} "
+                "-i $ssh_key_file $own_public_ip:{2}/{3} {4}"
+            )
         output(
-            "exec-start {0} scp-output-{1}-config.log stubborn-scp.sh {5} -i $ssh_key_file $own_public_ip:{2}/{3} {4}"
-            "".format(s, expID, MASTER_CONFIG_DIR, configFile, SLAVE_CONFIG_FILE, SCP_RETRY_COUNT)
+            scp_cmd.format(
+                s,
+                expID,
+                MASTER_CONFIG_DIR,
+                configFile,
+                SLAVE_CONFIG_FILE,
+                SCP_RETRY_COUNT,
+            )
         )
-        output("exec-wait {0} 60000 "
-               "exec-start {0} experiment-output/{1}/slave-__id__/FAILED echo Could not fetch config; "
-               "exec-wait {0} 2000".format(s, expID))
+        output(
+            "exec-wait {0} 60000 "
+            "exec-start {0} experiment-output/{1}/slave-__id__/FAILED echo Could not fetch config; "
+            "exec-wait {0} 2000".format(s, expID)
+        )
     for s in slaves:
         output("sync {0}".format(s))
     output("")
@@ -67,7 +85,9 @@ def pushLocalConfigFiles(expID, slaves):
     output("# Prepare config file.")
     for s, configFile in slaves.items():
         output(
-            "exec-start {0} /dev/null cp {1}/{2} experiment-output/{3}/slave-__id__/{4}".format(s, local_config_dir, configFile, expID, SLAVE_CONFIG_FILE)
+            "exec-start {0} /dev/null cp {1}/{2} experiment-output/{3}/slave-__id__/{4}".format(
+                s, local_config_dir, configFile, expID, SLAVE_CONFIG_FILE
+            )
         )
         output("exec-wait {0} 2000".format(s))
     for s in slaves:
@@ -164,10 +184,7 @@ def runClients(expID, clients):
                "exec-start {0} experiment-output/{1}/slave-__id__/FAILED echo Client failed or timed out; "
                "exec-wait {0} 2000".format(c, expID, timeout))
         output("sync {0}".format(c))
-        timeout //= 2 # In case of problems, avoids waiting for the full timeout for all sets of clients.
-                     # Not setting "timeout" directly to a very small value for all except the first set of clients
-                     # avoids the situation where the first set of clients exits normally and the second set of clients,
-                     # taking just slightly longer, is killed. The total timeout is still bounded by 2 * CLIENT_TIMEOUT
+        timeout //= 2
     output("")
 
 
@@ -268,8 +285,9 @@ def stopPeers(peers):
 def saveConfig(expID, slaves):
     output("# Save config file.")
     for s in slaves:
-        output("exec-start {0} /dev/null cp {1} experiment-output/{2}/slave-__id__".format(s, SLAVE_CONFIG_FILE,
-                                                                                                 expID))
+        output("exec-start {0} /dev/null cp {1} experiment-output/{2}/slave-__id__".format(
+            s, SLAVE_CONFIG_FILE, expID
+        ))
         output("exec-wait {0} 2000 "
                "exec-start {0} experiment-output/{1}/slave-__id__/FAILED echo Could not log config file; "
                "exec-wait {0} 2000".format(s, expID))
@@ -280,16 +298,31 @@ def submitLogs(expID, slaves):
     output("# Submit logs to master node")
     for s in slaves:
         output(
-            "exec-start {0} experiment-output/{1}/slave-__id__/submit-logs.log tar czf experiment-output-{1}-slave-__id__.tar.gz "
+            "exec-start {0} /dev/null tar czf experiment-output-{1}-slave-__id__.tar.gz "
             "experiment-output/{1}/slave-__id__".format(
                 s, expID))
         output("exec-wait {0} 30000 "
                "exec-start {0} experiment-output/{1}/slave-__id__/FAILED echo Could not compress logs; "
                "exec-wait {0} 2000".format(s, expID))
     for s in slaves:
+        if deplType == "remote":
+            scp_cmd = (
+                "exec-start {0} scp-output-{1}-logs.log stubborn-scp.sh {2} "
+                "experiment-output-{1}-slave-__id__.tar.gz $own_public_ip:{3}/raw-results/"
+            )
+        else:
+            scp_cmd = (
+                "exec-start {0} scp-output-{1}-logs.log stubborn-scp.sh {2} "
+                "-i $ssh_key_file experiment-output-{1}-slave-__id__.tar.gz $own_public_ip:{3}/raw-results/"
+            )
         output(
-            "exec-start {0} scp-output-{1}-logs.log stubborn-scp.sh {2} -i $ssh_key_file "
-            "experiment-output-{1}-slave-__id__.tar.gz $own_public_ip:{3}/raw-results/".format(s, expID, SCP_RETRY_COUNT, MASTER_EXP_DIR))
+            scp_cmd.format(
+                s,
+                expID,
+                SCP_RETRY_COUNT,
+                MASTER_EXP_DIR,
+            )
+        )
         output("exec-wait {0} 60000 "
                "exec-start {0} experiment-output/{1}/slave-__id__/FAILED echo Could not submit logs; "
                "exec-wait {0} 2000".format(s, expID))
@@ -320,6 +353,7 @@ def writeReadyFile():
     output("write-file $ready_file READY")
     output("")
 
+
 def writeLocalReadyFile():
     output("write-file master-ready READY")
     output("")
@@ -332,20 +366,14 @@ def generateCommands(expID, peers, clients):
     output("#========================================")
     output("\n")
 
-    # both peers and clients are dictionaries with slave tags as keys
-    # and the assigned config file names as the corresponding values.
-    # config is thus the assignment to slave tag to config file.
     config = peers.copy()
     config.update(clients)
 
     configFiles = {key: val[0] for key, val in config.items()}
     bandwidths = {key: val[1] for key, val in config.items()}
 
-    # slaves are all the tags of the used slaves, both peers and clients.
     slaves = list(peers) + list(clients)
 
-    # most of these functions only need the keys (slave tags)
-    # and only pushConfigFiles() needs the values (config files) too.
     waitForSlaves(slaves)
     createLogDir(expID)
     pushConfigFiles(expID, configFiles)
@@ -360,6 +388,7 @@ def generateCommands(expID, peers, clients):
 
     output("")
 
+
 def generateOldMirCommands(expID, peers, clients):
 
     output("#========================================")
@@ -367,26 +396,19 @@ def generateOldMirCommands(expID, peers, clients):
     output("#========================================")
     output("\n")
 
-    # both peers and clients are dictionaries with slave tags as keys
-    # and the assigned config file names as the corresponding values.
-    # config is thus the assignment to slave tag to config file.
     config = peers.copy()
     config.update(clients)
 
     configFiles = {key: val[0] for key, val in config.items()}
     bandwidths = {key: val[1] for key, val in config.items()}
 
-    # slaves are all the tags of the used slaves, both peers and clients.
     slaves = list(peers) + list(clients)
 
-    # most of these functions only need the keys (slave tags)
-    # and only pushConfigFiles() needs the values (config files) too.
     waitForSlaves(slaves)
     createLogDir(expID)
     pushConfigFiles(expID, configFiles)
     setBandwidth(expID, bandwidths)
     generateOldMirConfig(expID, list(peers), list(clients))
-    # startOldMirPeers(expID, list(peers))
     runOldMirClients(expID, list(clients))
     stopPeers(list(peers))
     unsetBandwidth(expID, bandwidths)
@@ -396,6 +418,7 @@ def generateOldMirCommands(expID, peers, clients):
 
     output("")
 
+
 def generateLocalCommands(expID, peers, clients):
 
     output("#========================================")
@@ -403,20 +426,14 @@ def generateLocalCommands(expID, peers, clients):
     output("#========================================")
     output("\n")
 
-    # both peers and clients are dictionaries with slave tags as keys
-    # and the assigned config file names as the corresponding values.
-    # config is thus the assignment to slave tag to config file.
     config = peers.copy()
     config.update(clients)
 
     configFiles = {key: val[0] for key, val in config.items()}
     bandwidths = {key: val[1] for key, val in config.items()}
 
-    # slaves are all the tags of the used slaves, both peers and clients.
     slaves = list(peers) + list(clients)
 
-    # most of these functions only need the keys (slave tags)
-    # and only pushConfigFiles() needs the values (config files) too.
     waitForSlaves(slaves)
     createLocalLogDir(expID)
     pushLocalConfigFiles(expID, configFiles)
@@ -460,29 +477,23 @@ def run(expID, tokens):
     global skipAllExisting
 
     runOldMir = False
-    # If oldmir is the first token, run old mir
     if tokens[0] == "oldmir":
         runOldMir = True
         tokens = tokens[1:]
 
-    # If special value "next" is specified as experiment ID, use the next value
     if expID == "next":
         expID = ("{:0"+str(experimentIdDigits)+"d}").format(idOffset)
         idOffset += 1
-    # Otherwise, set the global idOffset to continue counting from here
     else:
         experimentIdDigits = len(expID)
         idOffset = int(expID) + 1
 
-    # Allow user to skip existing experiments or to cancel the whole process
     skip = False
     outdir = "{0}/experiment-output/{1}".format(local_exp_data, expID)
 
     if os.path.isdir(outdir) and skipAllExisting:
         skip = True
     elif os.path.isdir(outdir):
-
-        # Ask the user what to do.
         sys.stderr.write("{0} already exists. (S)kip / skip (A)ll / (C)ancel? : ".format(outdir))
         sys.stderr.flush()
         answer = sys.stdin.readline().strip()
@@ -491,13 +502,12 @@ def run(expID, tokens):
             sys.stderr.flush()
             answer = sys.stdin.readline().strip()
 
-        # Act based on user's answer.
-        if answer == "s" or answer == "S":
+        if answer in {"s", "S"}:
             skip = True
-        elif answer == "a" or answer == "A":
+        elif answer in {"a", "A"}:
             skip = True
             skipAllExisting = True
-        elif answer == "c" or answer == "C":
+        elif answer in {"c", "C"}:
             os.exit("User abort.")
 
     clients = {}
@@ -506,13 +516,11 @@ def run(expID, tokens):
     bandwidth = defaultBandwidth
     role = None
 
-    # Process (tag, role) pairs.
     while tokens:
-
         if tokens[0] == "config:":
             config = tokens[1]
             tokens = tokens[2:]
-        if tokens[0] == "bandwidth:":
+        if tokens and tokens[0] == "bandwidth:":
             bandwidth = tokens[1]
             tokens = tokens[2:]
         elif tokens[0] == "peers:":
@@ -529,11 +537,12 @@ def run(expID, tokens):
                 else:
                     sys.exit("ic-parse-experiment.py: config file not found: {0}".format(configFile))
             else:
-                sys.exit("ic-parse-experiment.py: run {0}: must specify role and config before token '{1}'".format(expID, tokens[0]))
+                sys.exit("ic-parse-experiment.py: run {0}: must specify role and config before token '{1}'".format(
+                    expID, tokens[0]
+                ))
             tokens = tokens[1:]
 
     if not skip:
-        # TODO: Fix this dirty old mir hack!
         if runOldMir:
             generateOldMirCommands(expID, peers, clients)
         elif deplType in {"cloud", "remote"}:
@@ -541,7 +550,9 @@ def run(expID, tokens):
         elif deplType == "local":
             generateLocalCommands(expID, peers, clients)
         else:
-            sys.exit("generate-master-commands.py: unknown deployment type: {0} only know 'local' and 'cloud'".format(deplType))
+            sys.exit("generate-master-commands.py: unknown deployment type: {0} only know 'local' and 'cloud'".format(
+                deplType
+            ))
 
     lastFinished = expID
 
@@ -555,32 +566,28 @@ def printDeploymentSchedule():
 # main
 # ============================================================
 
-# get deployment type ("local" or "cloud")
 deplType = sys.argv[1]
 if deplType not in {"local", "cloud", "remote"}:
     sys.exit("generate-master-commands.py: first argument must be one of 'local', 'cloud', and 'remote'")
 
-# get input file name
-inFileName = sys.argv[2]
+# ajustar diretórios vistos pelos slaves quando estamos em modo "remote"
+if deplType == "remote":
+    MASTER_CONFIG_DIR = "iss/experiment-config"
+    MASTER_EXP_DIR = "iss/current-deployment-data"
 
-# open output file
+inFileName = sys.argv[2]
 outFile = open(sys.argv[3], "w")
 
-# Get local experiment data directories
 local_exp_data = sys.argv[4]
 local_config_dir = "config"
 
-# Initialize default values
 defaultConfig = ""
 defaultMachine = ""
 defaultBandwidth = "unlimited"
 
-# These are the default values, will be overridden by the explicitly set IDs.
 experimentIdDigits = 3
-idOffset=0
+idOffset = 0
 
-# Create "ready" file to indicate that the master server is running.
-# The deployment scripts wait until this file exists before starting slaves.
 if deplType == "local":
     writeLocalReadyFile()
 else:
@@ -588,8 +595,6 @@ else:
 
 with open(inFileName) as inFile:
     for line in inFile:
-
-        # Skip empty lines and comments.
         if line.strip() == "" or line.strip().startswith("#"):
             continue
 
