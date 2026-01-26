59d011d8783e1d1e472073fd3c5f4242196409f2
bruno.vdasilva <bruno.vdasilva@dataprev.gov.br>
2026-01-26 17:16:11 -0300

    corrigindo deploy

--- mirbft/deployment/scripts/experiment-configuration/generate-config.sh
+++ mirbft/deployment/scripts/experiment-configuration/generate-config.sh
@@ -68,7 +68,7 @@ checkpointers="Signing"
 
 # Parameters chosen for experiments
 durations="30"             # [s]   !!! Don't forget to change the timeout in generate-master-commands.py if increasing this value !!!
-drain_time_ms="5000"
+drain_time_ms="30000"
 bandwidths="unlimited" #"1gbit"         # any value accepted by the tc command or "unlimited" !!! ATTENTION: Adapt MaxProposeDataRate in config accordingly !!!
 payloadSizes="500"         # [Bytes]
 fixedEpochLength=false
