873d7c6d5c1ba1bad1ce4fafd9f89d0673885376
bruno.vdasilva <bruno.vdasilva@dataprev.gov.br>
2026-06-03 14:30:36 -0300

    clients1="16"

--- mirbft/deployment/scripts/experiment-configuration/generate-config.sh
+++ mirbft/deployment/scripts/experiment-configuration/generate-config.sh
@@ -38,7 +38,7 @@ machineLocations="fra05"
 faultyMachineLocations="sjc04 osa23 ams03 syd05 lon06 wdc07 che01 tok05 par01 dal10 fra05 mil01 mex01 tor01 tor04 seo01"
 
 # number of client instances per node for 1/16/32 client machines
-clients1="4"    # deploys 1 client machine which run the specified number of client instances
+clients1="16"    # deploys 1 client machine which run the specified number of client instances
 clients16=""    # deploys 16 client machine which run the specified number of client instances
 clients32=""    # deploys 32 client machine which run the specified number of client instances
 systemSizes="5" #"4 16 64 128"  Must be sorted in ascending order!
